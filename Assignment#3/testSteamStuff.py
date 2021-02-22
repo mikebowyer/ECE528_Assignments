12a9d9db582c515c93d68e23bc400f96f6cca041
Mike Bowyer <mbowyer@umich.edum>
2021-02-22 17:45:04 -0500

    Working data pulling from steam source

--- Assignment#3/testSteamStuff.py
+++ Assignment#3/testSteamStuff.py
@@ -10,14 +10,17 @@ if __name__ == '__main__':
         request = Request("https://steamcommunity.com/app/413150/allnews/", headers={'User-Agent': 'Mozilla/5.0'})
         source = urlopen(request).read()
         bsoup = soup(source, "html.parser")
-        main_blog_div = bsoup.findAll("div",{"id":"page1"})
+        announcments = bsoup.findAll("div",class_="apphub_Card Announcement_Card modalContentLink interactable")
     except:
         raise Exception("Couldn't connect to steams's website.")
 
     # Gets CSGO's patch title.
     try:
-        patch["title"] = main_blog_div[0].div.div.h2.a.contents[0]
-        if patch["title"] is None:
+        latestAnnouncment = announcments[0].findAll("div", class_="apphub_CardContentMain")
+        # patch["title"] = main_blog_div[0].div.div.h2.a.contents[0]
+        if False:
             raise Exception("Could not find  title.")
     except:
         raise Exception("Error retrieving  title.")
+
+
